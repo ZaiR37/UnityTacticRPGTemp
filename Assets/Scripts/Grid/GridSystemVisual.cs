f7a9c648938d0292bcf76d85ee143944b04b2ac5
ZaiR37 <70493871+ZaiR37@users.noreply.github.com>
2023-03-07 17:25:13 +0700

    Grid Visual Added

---
+++ Assets/Scripts/Grid/GridSystemVisual.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystemVisual : MonoBehaviour
+{
+    public static GridSystemVisual Instance { get; private set;}
+
+    [SerializeField] private Transform gridSystemVisualSinglePrefab;
+
+    private GridSystemVisualSingle[,] gridSystemVisualSingleArray;
+
+    private void Awake() {
+        if (Instance != null){
+            Debug.LogError("There's more than one GridSystemVisual! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }        
+        Instance = this;
+    }
+
+    private void Start() {
+        int gridWidth = LevelGrid.Instance.GetWidth();
+        int gridHeight = LevelGrid.Instance.GetHight();
+
+        gridSystemVisualSingleArray = new GridSystemVisualSingle[gridWidth, gridHeight];
+        for (int x = 0; x < gridWidth; x++){
+            for (int z = 0; z < gridHeight; z++){
+                GridPosition gridPosition = new GridPosition(x,z);
+                Transform gridSystemVisualSingleTransform =
+                    Instantiate(gridSystemVisualSinglePrefab, LevelGrid.Instance.GetWorldPosition(gridPosition), Quaternion.identity);
+                gridSystemVisualSingleArray[x,z] = gridSystemVisualSingleTransform.GetComponent<GridSystemVisualSingle>();
+            }
+        }
+    }
+
+    private void Update() {
+        UpdateGridVisual();
+    }
+
+    public void HideAllGridPosition(){
+        int gridWidth = LevelGrid.Instance.GetWidth();
+        int gridHeight = LevelGrid.Instance.GetHight();
+
+        for (int x = 0; x < gridWidth; x++){
+            for (int z = 0; z < gridHeight; z++){
+                gridSystemVisualSingleArray[x, z].Hide();
+            }
+        }
+    }
+
+    public void ShowGridPositionList(List<GridPosition> gridPositionList){
+        foreach(GridPosition gridPosition in gridPositionList){
+            gridSystemVisualSingleArray[gridPosition.x, gridPosition.z].Show();
+        }
+    }
+
+    public void UpdateGridVisual(){
+        Unit selectedUnit = UnitActionSystem.Instance.GetSelectedUnit();
+
+        HideAllGridPosition();
+        ShowGridPositionList(
+            selectedUnit.GetMoveAction().GetValidActionGridPositionList()
+        );
+    }
+}
--- Assets/Scripts/Grid/LevelGrid.cs
+++ Assets/Scripts/Grid/LevelGrid.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class LevelGrid : MonoBehaviour
 {
     public static LevelGrid Instance { get; private set;}
+
     [SerializeField] private Transform gridDebugObjectPrefab;
+    [SerializeField] private int gridWidth = 10;
+    [SerializeField] private int gridHeight = 10;
+    [SerializeField] private float gridCellSize = 2f;
 
     private GridSystem gridSystem;
 
@@ -17,10 +21,8 @@ public class LevelGrid : MonoBehaviour
         }        
         Instance = this;
 
-        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem = new GridSystem(gridWidth, gridHeight, gridCellSize);
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
-
-
     }
 
     public void AddUnitAtGridPosition(GridPosition gridPosition, Unit unit){
@@ -50,8 +52,10 @@ public class LevelGrid : MonoBehaviour
 
     public bool IsValidGridPosition(GridPosition gridPosition) => gridSystem.IsValidGridPosition(gridPosition);
 
-    public bool HasAnyUnitOnGridPosition(GridPosition gridPosition)
-    {
+    public int GetWidth() => gridSystem.GetWidth();
+    public int GetHight() => gridSystem.GetHight();
+
+    public bool HasAnyUnitOnGridPosition(GridPosition gridPosition){
         GridObject gridObject = gridSystem.GetGridObject(gridPosition);
         return gridObject.HasAnyUnit();
     }
